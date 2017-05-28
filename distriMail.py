4dd0a5670f1bec6ff9147f3e8ad7e5d9a842d300
alanjsil <alan__silveira@outlook.com>
2017-05-28 00:47:24 -0300

    Add files via upload

---
+++ distriMail.py
@@ -0,0 +1,67 @@
+__AUTHOR__	= "Getsu"
+__DATE__	= "28/05/2017"
+__VERSION__	= "1.0"
+__GITHUB__	= "https://github.com/alanjsil/MultiMail"
+__LICENÇA__ = "GNU General Public License v3.0"
+
+import banner
+import emails
+import os
+
+def limpa():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def interromper():
+    RED = '\33[91m'
+    YELLOW = '\33[93m'
+
+    limpa()
+    banner.banners.banner2()
+    print(RED+"[!]"+YELLOW+" VOCE CANCELOU!!!")
+    exit()
+
+def main():
+    RED = '\33[91m'
+    END = '\033[0m'
+
+    try:
+        banner.banners.banner1()
+        # Escolhas no menu principal
+        print("[1]GMAIL   [2]OUTLOOK   [3]SAIR")
+        try:
+
+            escolha = int(input(">>> "))
+            limpa()
+            banner.banners.banner1()
+
+            if escolha == 1:
+                emails.gmail()
+            elif escolha == 2:
+                emails.outlook()
+            elif escolha == 3:
+                interromper()
+            else:
+                limpa()
+                print(RED + '{--}ESCOLHA INCORRETA.{--}' + END)
+                main()
+        except ValueError:
+            limpa()
+            print(RED + '{--}ESCOLHA INCORRETA.{--}' + END)
+            print(RED + '  {--}NÃO USE LETRAS.{--}' + END)
+            main()
+
+    except KeyboardInterrupt:
+        interromper()
+
+def aviso():
+    try:
+        YELLOW = '\33[93m'
+        END = '\033[0m'
+        banner.banners.inicial()
+        NULL = input(YELLOW+"\nPressione ENTER para continuar."+END)
+        limpa()
+    except KeyboardInterrupt:
+        interromper()
+
+aviso()
+main()
